b918e23bb2e4ba1a4f33ebeb5ad4a776c9e04c35
frufrula <l.tgessner@gmail.com>
2018-07-05 11:52:49 -0500

    Actualización readme.md/JS/HTML

--- src/data.js
+++ src/data.js
@@ -1,1 +1,13 @@
-const labDataBase = "https://github.com/lindatablerosgessner/cdmx-2018-06-bc-core-am-data-dashboard/blob/master/data/laboratoria.json"
+const json = "https://raw.githubusercontent.com/lindatablerosgessner/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json"
+
+window.onload = () => {
+    fetch(json)
+    .then(response => response.json())    //toma el json
+    .then(data => {
+        console.log(data);
+        readyJson(data);
+    })
+    .catch(error => {
+        console.log("error");
+    })
+}
